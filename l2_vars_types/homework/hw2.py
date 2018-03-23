e0fead2c4da06077398b490bb27652a445d28db1
vidovgopol <ubn[f,2156Q>
2018-03-23 17:22:01 +0200

    Branch 1 commit

--- l2_vars_types/homework/hw2.py
+++ l2_vars_types/homework/hw2.py
@@ -2,3 +2,6 @@ age = int(input("How old are you?\n"))
 
 year = 2018 - age
 print(f"You were born at {year}")
+
+#For branch check
+Print("Branch check")
--- l2_vars_types/practise.py
+++ l2_vars_types/practise.py
@@ -1,1 +1,30 @@
 print("Hi")
+
+# List or spisok
+my_guest = ["Alina", "Ruslan", "Tanya"]
+type(my_guest)
+
+#Dict
+my_dict = {"1":"name", "2":"lastname"}
+type(my_dict)
+
+#Tuple
+my_tuple = ("Tup1", "Tup2")
+type(my_tuple)
+
+
+
+name = "Vova"
+last_name = "Dovgopol"
+
+result = f"Your name is {name}, your last name is {last_name}" # P3.6
+
+print(result)
+
+result = "Your name is %s, your last name is %s" % (name, last_name)
+
+print(result)
+
+result = "Your name is {0}, your last name is {1}".format(name,last_name)
+
+print(result)
