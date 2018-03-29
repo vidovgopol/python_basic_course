34d765dc4992857c77de6f5d0cae456f9969a1f3
vidovgopol <ubn[f,2156Q>
2018-03-29 10:58:45 +0300

    HW3 Commit2

--- l3_conditional_flow/homework/homework Part2.py
+++ l3_conditional_flow/homework/homework Part2.py
@@ -1,18 +1,46 @@
 print("Enter your credit card please:")
-card_num = input("Enter credit card number: ")
+
+#Card number section
+card_num = input("Enter credit card number:\n")
+
+#Check for digit in card number
+try:
+    int(card_num)
+except ValueError:
+    print("Error card number contains digit")
+    exit()
+
+#Check for card number lenght
 card_num_len = len(card_num)
 if card_num_len != 16:
+    print("Card number incorrect")
     exit()
 else:
-    exp_date = input("Enter expiration date in format (dd/yy):")
-    try:
+    pass
+
+#Expiration date section
+exp_date = input("Enter expiration date in format (dd/yy):\n")
+try:
         int(exp_date)
+        print("Expiration date incorect")
         exit()
-    except ValueError:
+except ValueError:
         print("Ok")
 
-cvv_code = input("Enter CVV code: ")
+
+#CVV code section
+cvv_code = input("Enter CVV code:\n")
 cvv_code_len = len(cvv_code)
+
+#Check for digit in CVV
+
+try:
+    int(cvv_code)
+except ValueError:
+    print("CVV code contains digit")
+    exit()
+
+#Check for CVV leght
 if cvv_code_len < 3:
     print("CVV code incorrect")
 else:
