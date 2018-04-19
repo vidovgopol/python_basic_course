a93e3a87089905093a63d4d200e0e1c8bcae207b
vidovgopol <ubn[f,2156Q>
2018-04-19 17:38:10 +0300

    Comm

--- final_project/app.py
+++ final_project/app.py
@@ -4,16 +4,27 @@ from utils import check_holder_name
 from utils import check_exp_date
 from utils import check_phone_number
 
+import csv
 
 card_holders_coll = card_holders_generation()
 print(card_holders_coll)
 current_year = 18
+card_holders_coll_no_errors = []
+card_holders_coll_with_errors = []
 for el in card_holders_coll:
     card_holder_name_correct = check_holder_name(el['Name'])
     card_phone_correct = check_phone_number(el['Phone'])
     card_exp_date_correct = check_exp_date(el['Exp_day'], current_year)
     card_number_correct = check_credit_card(el['Card'])
     if card_phone_correct and card_number_correct:
-        print("Ok")
+        card_holders_coll_no_errors.append(el)
     else:
-        print("Bad")
+        card_holders_coll_with_errors.append(el)
+
+
+test = {'name': 'vova', 'Phone': '88888'}
+with open('cardholders.csv', 'w', newline='') as csv:
+    header = ['Name', 'Phone']
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    writer.writeheader()
+    writer.writerow(test)
